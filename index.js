84497bed7701cb6278d245e176111122060f2a61
zeena kareem <zena34269@gmail.com>
2020-06-10 12:39:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here
 function findMatching(drivers,str){
     let arr=[]
-  arr=drivers.filter((driver) => driver===str,str)
+  arr=drivers.filter((driver) => driver.toLowerCase()===str.toLowerCase(),str)
   return arr
   }
