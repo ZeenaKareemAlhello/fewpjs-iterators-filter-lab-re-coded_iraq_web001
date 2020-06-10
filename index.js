aaf1755197105526dd1468a4bb9d75cda0e17f08
zeena kareem <zena34269@gmail.com>
2020-06-10 12:13:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+function findMatching(drivers,str){
+  let arr=[]
+  for(const driver in drivers){
+  if(driver===str)arr.push(driver)
+}}
+
+findMatching(['zeena','teba','rabe'],'zeena')
